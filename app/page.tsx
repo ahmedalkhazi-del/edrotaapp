517542a09ab182b615ad24e1f4f3d8b79b79652a
ahmedalkhazi-del <ahmedalkhazi@gmail.com>
2026-05-24 02:43:41 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from 'next/navigation'
 import { createClient } from '@/lib/supabase-server'
+
 export default async function Home() {
   const supabase = createClient()
   const { data: { user } } = await supabase.auth.getUser()
